1a5380eebf77a00b9327f9056360d1fa83b6c939
Saged Ryan <sagedryan775@gmail.com>
2023-12-04 13:43:57 -0800

    saged

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """
-    Print a list of integers
-    ...
-
-    Parameters
-    ----------
-    my_list : list optional
-        The list of integers
-
-    Return:
-        None
-    """
-
-    for element in my_list:
+def print_list_integer(my_list=[])
+    for x in my_list:
         print("{:d}".format(element))
